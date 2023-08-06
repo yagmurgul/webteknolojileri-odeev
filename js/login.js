b52ff242e859117533007c4081d892f10b27db40
yagmurgul <91977186+yagmurgul@users.noreply.github.com>
2023-08-06 16:08:55 +0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function loginDenetle(){
+    var kulAd="b211210555";
+    var sifre="12345";
+    var KullaniciAdi=document.getElementById("kullaniciAdi").value;
+    var Parola=document.getElementById("parola").value;
+    var Hata=document.getElementById("hata");
+
+    if(KullaniciAdi=="" || Parola==""){
+        Hata.style.visibility="visible";
+        Hata.innerHTML="Kullanıcı adı ya da Parola boş bırakılamaz.";
+        return false;
+    }
+    else if((KullaniciAdi!="" && KullaniciAdi!=kulAd) || (Parola!="" && Parola!=sifre)){
+        Hata.style.visibility="visible";
+        Hata.innerHTML="Kullanıcı adı ya da Parola yanlış.";
+        return false;
+    }
+    else{
+        return true;
+    }
+}
